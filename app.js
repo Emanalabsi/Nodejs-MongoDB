2fbd67d94a79506e25b4e6eb8bdebe1f6110b78d
Emanalabsi <emanabsi30@gmail.com>
2023-05-12 21:43:53 +0300

    feat(api): Add MongoDB integration for posts data storage

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const router = require("./api/routes/posts");
+const router = require("./api/routes/posts.routes");
 
 app.use(express.json());
+
 app.use(router);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require("./app");
+const dbConnection = require("./api/config/db.connection");
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`server is listening on: http://localhost:${port}`);
-});
+dbConnection.then(
+  app.listen(port, () => {
+    console.log(`server is listening on: http://localhost:${port}`);
+  })
+);
